858b15e07635fcfae5cc143143d598b550c5d988
bbaing4564 <bbaing4564@gmail.com>
2021-04-11 21:12:01 +0900

    Initial commit

---
+++ src/main/java/com/devbaek/webfluxr2dbcmariadb/WebfluxR2dbcMariadbApplication.java
@@ -0,0 +1,13 @@
+package com.devbaek.webfluxr2dbcmariadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxR2dbcMariadbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxR2dbcMariadbApplication.class, args);
+    }
+
+}
